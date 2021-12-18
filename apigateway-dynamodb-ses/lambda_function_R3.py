184698db8e71c634083e1085a0140aad594bacae
miyahara <50367145+subretu@users.noreply.github.com>
2021-12-18 23:30:51 +0900

    SESでメールを送信するlambda作成

--- apigateway-dynamodb-ses/lambda_function_R3.py
+++ apigateway-dynamodb-ses/lambda_function_R3.py
@@ -7,6 +7,36 @@ import decimal
 
 dynamodb = boto3.resource("dynamodb")
 
+# 送信元アドレス
+MAILFROM = 'xxxxxx'
+
+
+def sendmail(to, subject, body):
+    client = boto3.client('ses')
+
+    response = client.send_email(
+        Source=MAILFROM,
+        ReplyToAddresses=[MAILFROM],
+        Destination={
+            # 宛先アドレス
+            'ToAddresses': [
+                'yyyyyy'
+            ]
+        },
+        Message={
+            'Subject': {
+                'Data': subject,
+                'Charset': 'UTF-8'
+            },
+            'Body': {
+                'Text': {
+                    'Data': body,
+                    'Charset': 'UTF-8'
+                }
+            }
+        }
+    )
+
 
 def next_seq(table, tablename):
     response = table.update_item(
@@ -31,6 +61,14 @@ def lambda_handler(event, context):
         host = event["requestContext"]["identity"]["sourceIp"]
         now = time.time()
 
+        s3 = boto3.client("s3")
+        url = s3.generate_presigned_url(
+            ClientMethod="get_object",
+            Params={"Bucket": "lambda-secret-test", "Key": "Earth.png"},
+            ExpiresIn=48 * 24 * 60 * 60,
+            HttpMethod="GET",
+        )
+
         usertable = dynamodb.Table("user")
         usertable.put_item(
             Item={
@@ -39,8 +77,20 @@ def lambda_handler(event, context):
                 "email": email,
                 "accepted_at": decimal.Decimal(str(now)),
                 "host": host,
+                "url": url,
             }
         )
+
+        mailbody = """
+        {0}様
+
+        ご登録ありがとうございました。
+        下記のURLからダウンロードできます。
+
+        {1}
+        """.format(username, url)
+
+        sendmail(email, "登録ありがとうございました", mailbody)
 
         return {
             "statusCode": 200,
@@ -55,5 +105,5 @@ def lambda_handler(event, context):
         return {
             "statusCode": 500,
             "headers": {"content-type": "text/html"},
-            "body": '<!DOCTYPE html><html><head><meta charset="UTF-8"></head><body内部エラーが発生しました。</body></html>',
+            "body": '<!DOCTYPE html><html><head><meta charset="UTF-8"></head><body>内部エラーが発生しました。</body></html>',
         }
